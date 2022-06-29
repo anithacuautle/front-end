d659f187ec76f24e5de82f7b5cacdc191ffa6958
anithacuautle <cuautleanayeli3@gmail.com>
2022-06-29 00:06:20 -0500

    commint

--- pages/login.js
+++ pages/login.js
@@ -4,16 +4,15 @@ export default function dual() {
   return (
    <div className={styles.hero}>
       <div className={styles.log_cont}>
-          <img src='https://img2.freepng.es/20180531/lwc/kisspng-dietitian-health-nutrition-food-nutritionist-5b0fb241c63919.6045298015277553298119.jpg' className={styles.is}></img>
+          <img src="nutrina1.png" className={styles.is}></img>
           <div className={styles.log_in_cont}>
-              <h1 className={styles.tittle}>Nutrína</h1>
-
+              <br></br>
               <form className={styles.inputs_con}>
                   <input className={styles.input} type="text" placeholder="Nombre del usuario"></input>
                   <input className={styles.input} type="password" placeholder="Password"></input>
                   <button className={styles.btn} href="#">Iniciar</button><br></br>
                   <a href='#' className={styles.men}>Haz olvidado tu contraseña?</a><br></br>
-                  <a href='#'className={styles.men}>Don't have an account?</a>
+                  
               </form>
           </div>
       </div></div>
